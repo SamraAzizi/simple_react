0d13129cc8c9f25d5d9950466f915b6492c777fb
SamraAzizi <samarraazizi@gmail.com>
2024-03-30 17:57:39 +0500

    my commit

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import data from "./data";
 
+export default function Accordion() {
+    const [selected, setSelected] = useState(null);
 
-export default function Accordion(){
-    return <div>Accordion</div>;
+    function handleSingleSelection(getCurrentId) {
+        console.log(getCurrentId)
+        setSelected(getCurrentId)
+    }
+    return
+    <div className="wrapper"><div className="accordion">{
+        data && data.lenght > 0 ?
+            data.map(dataItem => <div className="item">
+                <div onClick={handleSingleSelection(dataItem.id)} className="title">
+                    <h3>{dataItem.question}</h3>
+                    <span>+</span>
+                </div>{
+                    selected === dataItem.id ?
+                        <div className="content">{dataItem.answer}</div>
+                        : null
+                }
+
+            </div>)
+            : <div>No data found </div>
+    }
+    </div></div>;
 }
