fbe68d04f9deab1babeb8238eb1c866cfde2f056
SamraAzizi <fahimaazizi369@gmail.com>
2024-04-05 18:18:23 +0500

    my commit

--- src/components/random_color/index.jsx
+++ src/components/random_color/index.jsx
@@ -5,6 +5,28 @@ import { useState } from "react";
 export default function RamdomColor(){
     const [typeOfColor, setTypeOfColor] = useState('hex');
     const [color, setColor] = useState('#000000')
+
+    function randomColorUtility(length){
+        return Math.floor(Math.random()*length)
+
+    }
+    
+    function handCreateRandomHexColor(){
+        const hex = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+        let hexColor = '#';
+
+        for(let i=0;i<6;i++){
+            hexColor += hex[randomColorUtility(hex.length)]
+        }
+
+        
+
+    }
+
+
+    function handCreateRandomRgbColor(){
+
+    }
     return (
         <div styel={{
             width:'100vw',
@@ -13,9 +35,9 @@ export default function RamdomColor(){
         }
              
         }>
-            <button>Create HEX color</button>
-            <button>Create RGB color</button>
-    <button>Generate Color</button>
+            <button onClick={()=> setTypeOfColor('hex')}>Create HEX color</button>
+            <button onClick={()=> setTypeOfColor('rgb')}>Create RGB color</button>
+    <button onClick={typeOfColor === 'hex' ? handCreateRandomHexColor : handCreateRandomRgbColor}>Generate Color</button>
     </div>
     );
 } 
