aabc4992eff0db1e89e5a70f62aec8bc851d5860
SamraAzizi <samarraazizi@gmail.com>
2024-04-07 18:29:36 +0500

    my commit

--- src/components/image_slider/index.jsx
+++ src/components/image_slider/index.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react"
 
 
 export default function ImageSlider(url, limit){
+
+    const[image, setImage] = useState([]);
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    async function fectchImages(getUrl){
+
+    }
+
+    useEffect(()=>{
+        if(url !== '')  fectchImages()
+
+    },[url] )
+
+
+    return <div className="container"></div>
     
 }
