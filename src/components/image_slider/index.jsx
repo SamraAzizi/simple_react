28448c9e9280ea26ed4aed896e67dbc176b62ebb
SamraAzizi <fahimaazizi369@gmail.com>
2024-04-08 21:10:29 +0500

    my commit

--- src/components/image_slider/index.jsx
+++ src/components/image_slider/index.jsx
@@ -6,14 +6,22 @@ export default function ImageSlider(url, limit){
     const[image, setImage] = useState([]);
     const [currentSlide, setCurrentSlide] = useState(0);
     const [error, setError] = useState(null);
+    const [loading, setLoading ] = useState(false);
+
+
 
     async function fectchImages(getUrl){
         try{
             const response = await fetch(getUrl);
             const data = await response.json();
+            if(data){
+                setImage(data)
+                setLoading(false)
+            }
 
         }catch(e){
             setError(e.message);
+            setLoading(false)
 
         }
 
