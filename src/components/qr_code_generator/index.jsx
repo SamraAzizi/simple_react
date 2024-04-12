8e3877ff37fe090e6eb92b73a868aae93ac4fcfc
SamraAzizi <samarraazizi@gmail.com>
2024-04-12 20:05:23 +0500

    my commit

---
+++ src/components/qr_code_generator/index.jsx
@@ -0,0 +1,17 @@
+
+
+
+export default function QRCode(){
+
+
+
+
+    return <div>
+        <h1>QR Code Generator</h1>
+        <div>
+            <input type="text"/>
+            <button>Generate</button>
+        </div>
+    </div>
+
+}
