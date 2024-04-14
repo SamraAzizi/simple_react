10a66b1d12d799806e95055d279203fc33d2e8c6
SamraAzizi <samarraazizi@gmail.com>
2024-04-14 13:37:02 +0500

    my commit

--- src/components/light_dark_mode/index.jsx
+++ src/components/light_dark_mode/index.jsx
@@ -1,15 +1,23 @@
+import useLocalStorage from "./useLocalStorage"
 
 
 
 export default function LightDarkMode(){
 
+    const [theme, setTheme] = useLocalStorage('theme','dark')
+
+    function handletoggleTheme(){
+        setTheme(theme === 'light' ? 'dark' :'light')
+
+    }
+
     return <div className="light-dark-mode">
         <div className="container">
             <p>Asalamalaikum Everyone</p>
-            <button>Change The Theme!</button>
+            <button onClick={handletoggleTheme}>Change The Theme!</button>
 
         </div>
 
     </div>
 
-}
+} 
