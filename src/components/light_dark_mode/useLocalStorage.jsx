541eca0d90d0609d143bc59ef6169bd693352133
SamraAzizi <fahimaazizi369@gmail.com>
2024-04-14 13:03:21 +0500

    my commit

---
+++ src/components/light_dark_mode/useLocalStorage.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+
+
+export default function useLocalStorage(key, defaultValue){
+    const [value, setValue] = useState(()=>{
+        let currentValue;
+
+        try{
+            currentValue = JSON.parse(localStorage.getItem(key) || String(defaultValue))
+
+        }catch(error){
+            currentValue = defaultValue
+
+        }
+        return currentValue
+
+    }
+)
+
+
+useEffect(()=>{
+
+},{key,value})
+}
