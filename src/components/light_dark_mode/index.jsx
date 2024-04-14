3bb7088f69101c4d7d3db42e89ac0e63eac6b7ed
SamraAzizi <samarraazizi@gmail.com>
2024-04-14 12:38:15 +0500

    my commit

---
+++ src/components/light_dark_mode/index.jsx
@@ -0,0 +1,15 @@
+
+
+
+export default function LightDarkMode(){
+
+    return <div className="light-dark-mode">
+        <div className="container">
+            <p>Asalamalaikum everyone</p>
+            <button>Change The Theme!</button>
+
+        </div>
+
+    </div>
+
+}
