6c0b8bd28cdcf3dae5ba4b00026ffbcd3d986a00
SamraAzizi <samarraazizi@gmail.com>
2024-04-17 13:15:04 +0500

    my commit

--- src/components/github_profile_finder/index.jsx
+++ src/components/github_profile_finder/index.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react"
 
 
 export default function GitHubFinder(){
+    const [userName, setUserName] = useState('SamraAzizi')
 
+    async function fetchGithubUserData(){
+        const res = await fetch(`http://api.github.com/users/${'userName'}`)
+
+        const data = await res.json()
+
+    }
+
+    function handleSubmit(){
+
+    }
+
+    useEffect(()=>{
+        fetchGithubUserData()
+    }, [])
 
     return <div className="github-profile-container">
 
@@ -10,8 +26,13 @@ export default function GitHubFinder(){
             name="search-by-usename"
             type="text"
             placeholder="Search Github Username..."
+            value={userName}
+            onChange={(event) => setUserName(event.target.value)}
             
+            
+
             />
+            <button onClick={handleSubmit}>Search</button>
 
         </div>
     </div>
