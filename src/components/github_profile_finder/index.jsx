fefcfcb688fe4afe354d4f807f34348ebdf584b3
SamraAzizi <fahimaazizi369@gmail.com>
2024-04-17 13:22:32 +0500

    my commit

--- src/components/github_profile_finder/index.jsx
+++ src/components/github_profile_finder/index.jsx
@@ -3,11 +3,19 @@ import { useEffect, useState } from "react"
 
 export default function GitHubFinder(){
     const [userName, setUserName] = useState('SamraAzizi')
+    const [userData, setUserData] = useState(null)
+    const [loading , setLoading] = useState(false)
 
     async function fetchGithubUserData(){
+        setLoading(true)
         const res = await fetch(`http://api.github.com/users/${'userName'}`)
 
         const data = await res.json()
+        if(data){
+            setUserData(data)
+            setLoading(false)
+
+        }
 
     }
 
@@ -18,6 +26,11 @@ export default function GitHubFinder(){
     useEffect(()=>{
         fetchGithubUserData()
     }, [])
+
+    if(loading){
+        return <h1>Loading Data ! please wait.</h1>
+
+    }
 
     return <div className="github-profile-container">
 
