cc0fba108662263d60e3d4ba77712d20401aef8f
SamraAzizi <fahimaazizi369@gmail.com>
2024-04-17 12:59:20 +0500

    my commit

---
+++ src/components/github_profile_finder/index.jsx
@@ -0,0 +1,19 @@
+
+
+export default function GitHubFinder(){
+
+
+    return <div className="github-profile-container">
+
+        <div className="input-wrapper">
+            <input
+            name="search-by-usename"
+            type="text"
+            placeholder="Search Github Username..."
+            
+            />
+
+        </div>
+    </div>
+    
+}
