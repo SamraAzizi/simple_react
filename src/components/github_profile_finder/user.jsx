aab5d75e8eb7efd0f66cb219f5ff1d36b5b0706c
SamraAzizi <fahimaazizi369@gmail.com>
2024-04-19 21:04:29 +0500

    my commit

--- src/components/github_profile_finder/user.jsx
+++ src/components/github_profile_finder/user.jsx
@@ -1,43 +1,44 @@
-
-
-
 export default function User({ user }) {
-    const { avator_url, followers, following, public_reports, name, login, created_at } = user;
-
-    const createdDate = new Data(created_at)
-
-    return <div className="user">
+    const {
+      avatar_url,
+      followers,
+      following,
+      public_repos,
+      name,
+      login,
+      created_at,
+    } = user;
+  
+    const createdDate = new Date(created_at);
+  
+    return (
+      <div className="user">
         <div>
-            <img src="avator_url" className="avator" alt="User" />
-
+          <img src={avatar_url} className="avatar" alt="User" />
         </div>
-        <div>
-
-            <a href={`https"//github.com/${login}`}>{name || login}</a>
-
-            <p>User joined on {`${createdDate.getDate()} ${createdDate.toLocalString('en-us', { month: 'short' })} ${createdDate.getFullYear()}`}</p>
-
-
+        <div className="name-container">
+          <a href={`https://github.com/${login}`}>{name || login}</a>
+          <p>
+            User joined on{" "}
+            {`${createdDate.getDate()} ${createdDate.toLocaleString("en-us", {
+              month: "short",
+            })} ${createdDate.getFullYear()}`}
+          </p>
         </div>
-        <div>
-
-            <div>
-                <p>Public repos</p>
-                <p>{public_reports}</p>
-
-            </div>
-            <div>
-                <p>followers</p>
-                <p>{followers}</p>
-
-            </div>
-            <div>
-                <p>following</p>
-                <p>{following}</p>
-            </div>
+        <div className="profile-info">
+          <div>
+            <p>Public Repos</p>
+            <p>{public_repos}</p>
+          </div>
+          <div>
+            <p>Followers</p>
+            <p>{followers}</p>
+          </div>
+          <div>
+            <p>Following</p>
+            <p>{following}</p>
+          </div>
         </div>
-
-
-    </div>
-
-}
+      </div>
+    );
+  }
